840e22ba2088b2c87717aaf130b333b8ff60a1b1
sugyinbrs <sgnglee@gmail.com>
2022-06-05 03:12:27 +0900

    Add: socket.io 인스턴스 생성 후 코드 추가

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+/* require()
+- Node JS 에서 별도의 파일에 존재하는 외부 모듈을 포함하는 내장 함수
+- 기본적으로 JS 파일을 읽어서 실행한 후 내보내기 객체를 반환하는 작업을 진행
+- 내장된 핵심 Node JS 모듈 뿐만 아니라 커뮤니티 기반 및 로컬 모듈을 추가할 수 있음
+*/
+
 const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server); // server 객체를 전달하면서 새로운 socket.io 인스턴스 초기화
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 server.listen(3000, () => {
